d4ecac225c44be2cf4262f95abe92bbf75e3c76f
MahmoudKhalidShabaanOmar <m.k.s.omar1112003@gmail.com>
2024-12-01 11:40:39 +0300

    Add files via upload

--- problem_43.py
+++ problem_43.py
@@ -1,9 +1,12 @@
-# write a python program to get user name , and user password and make sure that password only contains alphabetic , or numeric characters , and the length of the user password is less than or equal to 8 =>
-user_name = input("please enter the name of the user is : ")
-user_password = input("please enter the password of the user is : ")
-print("the name of the user is : \""+user_name+"\"")
-print("the password of the user is : \""+user_password+"\"")
-if((user_password . isalnum()) and (len(user_password) <= 8)) :
-    print("HI \""+user_name+"\" , your user password is OKAY , because it contains numeric , or alphabetic characters , and the length of the user password is less than , or equal to eight , because the user password is : \""+user_password+"\" , and the length of the user password is : \""+str(len(user_password))+" characters\"")
-else :
-    print("SORRY \""+user_name+"\" , your user password is NOT-OKAY , because it contains numeric , or alphabetic characters , and it also contains special characters, and the length of the password is grater than eight , because the user password is : \""+user_password+"\" , and the length of the user password is : \""+str(len(user_password))+" characters\"")
+# write a python program to get six numbers in the list , and then find the sum of that numbers =>
+# lista_of_numbers = []
+# for i in range(6) :
+    # lista_of_numbers . append(int(input("please enter the numbers to store in the list of the numbers is = ")))
+lista_of_numbers = [int(input("please enter the numbers to store in the list of the numbers is = ")) for i in range(6)]
+print("the list of the numbers is = "+str(lista_of_numbers))
+print("the length of the list of the numbers is = "+str(len(lista_of_numbers))+" numbers")
+# sum_of_all_numbers = 0 
+# for j in lista_of_numbers :
+    # sum_of_all_numbers += j
+sum_of_all_numbers = sum(lista_of_numbers) 
+print("the sum of the all numbers in the list is = "+str(sum_of_all_numbers))
